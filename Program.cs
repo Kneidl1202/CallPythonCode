d1534ac9b0cf9b5ba0127662ba981f9d58973547
Kneidl1202 <andreas.auer03@gmail.com>
2020-10-22 22:36:42 +0200

    create Main

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace callPythonCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        void CallPythonProgramm()
+        {
+            string command = "";
+            System.Diagnostics.Process.Start("CMD.exe", command);
+        }
+    }
+}
