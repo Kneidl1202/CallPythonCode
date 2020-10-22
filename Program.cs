d9d373500f5cb0c0781c6d6da14ed534e4147122
Kneidl1202 <andreas.auer03@gmail.com>
2020-10-22 23:09:51 +0200

    add selectDirectories and made some changes in main programm

--- Program.cs
+++ Program.cs
@@ -11,8 +11,23 @@ namespace callPythonCode
 
         void CallPythonProgramm()
         {
-            string command = "";
+            string path = Environment.CurrentDirectory;
+            string command = path + "/" + "main.py";
             System.Diagnostics.Process.Start("CMD.exe", command);
+        }
+
+        void CopyFilesToFlashDrive()
+        {
+            string amount = "amount.txt";
+            string originalPage1 = "seite1.txt";
+            string originalPage2_1 = "seite2checkboxes.txt";
+            string originalPage2_2 = "seite2textboxes.txt";
+            string originalPage_3 = "seite3.txt";
+            string originalPage_4 = "seite4.txt";
+            string originalPage_5 = "seite5.txt";
+            string originalPage_6 = "seite6.txt";
+
+
         }
     }
 }
